2afe0e23468f695cff00cb87a92a6c8feea1dcd1
kevin-liem <reeden88@gmail.com>
2015-06-22 20:47:33 +0700

    Upload Unity Project
    All Asset All Script

---
+++ Base Defense (Zombie)/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public float y1Pos;
+    public float y2Pos;
+    public float xPos;
+    public float wavesWait;
+
+    public GameObject Enemy;
+
+	void Start () {
+	    StartCoroutine(EnemySpawning());
+	}
+
+    IEnumerator EnemySpawning(){
+        while(true){
+            Vector3 InstantiatePos = new Vector3(xPos, Random.Range(y1Pos, y2Pos), 0f);
+            Instantiate(Enemy, InstantiatePos, Quaternion.identity);
+            yield return new WaitForSeconds(wavesWait);
+        }
+    }
+}

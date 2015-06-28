9283e8a81899553fc2b3773b4b2109bc35151aea
rom-bar <rommy.setiandy@gmail.com>
2015-06-28 10:28:04 +0700

    Fix Enemy Movement
    Now, Enemy cycle like this :1.random placed 2. move to the left. 3. if
    enemy hit invisible wall, enemy will move toward door/gate.

--- Base Defense (Zombie)/Assets/Scripts/EnemyMovement.cs
+++ Base Defense (Zombie)/Assets/Scripts/EnemyMovement.cs
@@ -3,25 +3,35 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour {
 
-    public float speed = 1f;
+	public float speed = 1f;
     private bool ableMove = true;
 	private Transform targetBase;
+	public Transform sightStart, sightEnd;
+	private bool moveToDoor = false;
 
 	void Start(){
-			targetBase = GameObject.FindGameObjectWithTag ("Base").transform;
-	}
+		targetBase = GameObject.FindGameObjectWithTag ("Base").transform;
+	} 
 
-	// Update is called once per frame
 	void Update () {
 		if(ableMove){
-			//            gameObject.transform.Translate(Vector3.left * Time.deltaTime * speed);
 			float step = Time.deltaTime * speed;
-			transform.position = Vector3.MoveTowards(transform.position, targetBase.position, step);
+			//use LineCast to see if enemy get contact to invisWall
+			//if got contact to invisWall LayerMask then enemy will move to door instead move toward left
+			moveToDoor = Physics2D.Linecast (sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer ("InvisWall"));
+			//un-Comment debug below to see LineCast on Enemy
+			//Debug.DrawLine (sightStart.position, sightEnd.position, Color.green);		
+			if(moveToDoor){
+				//un-Comment debug below to see if Enemy will move to door
+				//Debug.Log("Enemy Move to Door");	
+				transform.position = Vector3.MoveTowards(transform.position, targetBase.position, step);				
+			}else{
+				gameObject.transform.Translate(Vector3.left * Time.deltaTime * speed);
+			}
 		}
 	}
 
     void OnTriggerEnter2D(Collider2D other) {
-//		ableMove = false;
 		if(other.tag == "Base"){
 			GameManager.instance.AttackBase();
 			Destroy(gameObject);

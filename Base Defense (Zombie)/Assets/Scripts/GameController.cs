3031171e2069217449a6d69d87f73d73d097a9a5
zipzingame <zipzingame@gmail.com>
2015-07-01 23:56:19 +0700

    remove unused door variable

--- Base Defense (Zombie)/Assets/Scripts/GameController.cs
+++ Base Defense (Zombie)/Assets/Scripts/GameController.cs
@@ -7,7 +7,6 @@ public class GameController : MonoBehaviour {
     public float y2Pos;
     public float xPos;
     public float wavesWait;
-	public GameObject door;
     public GameObject Enemy;
 
 	void Awake(){
@@ -21,8 +20,6 @@ public class GameController : MonoBehaviour {
 
     IEnumerator EnemySpawning(){
         //Enemy spawns when Base HP not zero
-		Door scriptDoor = (Door) door.GetComponent<Door>();
-		Debug.Log("HP nya "+scriptDoor.hitPoints);
 		while(true){
             Vector3 InstantiatePos = new Vector3(xPos, Random.Range(y1Pos, y2Pos), 0f); //Set spawning position
             Instantiate(Enemy, InstantiatePos, Quaternion.identity);

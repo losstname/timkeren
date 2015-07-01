a63a62655cb54de631076629b41612b95758503c
zipzingame <zipzingame@gmail.com>
2015-07-02 05:54:16 +0700

    fix error file not added into version control

---
+++ Base Defense (Zombie)/Assets/Scripts/Deprecated/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
+	public int hitPoints ;
+	private Text levelText;
+	private Text attackText;
+	private Text destroyText;
+	private GameObject baseDoor;
+	public bool attackable = false;
+	public bool destroyable = false;
+
+	void Awake()
+	{
+		if (instance == null)
+
+		//if not, set instance to this
+		instance = this;
+
+		//If instance already exists and it's not this:
+		else if (instance != this)
+
+		//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+		Destroy(gameObject);
+
+		//Sets this to not be destroyed when reloading scene
+		//DontDestroyOnLoad(gameObject);
+		InitGame();
+	}
+
+	void InitGame()
+	{
+		hitPoints = 10;
+		attackable = true;
+		destroyable = true;
+		levelText = GameObject.Find("BaseHPText").GetComponent<Text>();
+		baseDoor = GameObject.Find("Door");
+		levelText.text = "Base HP: " + hitPoints;
+	}
+
+	public void AttackBase(){
+		if(attackable){
+			if(levelText == null)
+				levelText = GameObject.Find("BaseHPText").GetComponent<Text>();
+			hitPoints--;
+			if(hitPoints >0)
+				levelText.text = "Base HP:  " + hitPoints;
+			else{
+				levelText.text = "You Lose ";
+				if(destroyable){
+					baseDoor.SetActive(false);
+				}
+			}
+		}
+	}
+
+	public void restartScene(){
+		InitGame();
+	}
+}
--- Base Defense (Zombie)/Assets/Scripts/ProjectileLinePatternMovement.cs
+++ Base Defense (Zombie)/Assets/Scripts/ProjectileLinePatternMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ProjectileLinePatternMovement : MonoBehaviour {
 
-    public float speed = 10;
+    public float speed = 10f;
 
 	void Update () {
         transform.Translate(Vector2.right * Time.deltaTime * speed);

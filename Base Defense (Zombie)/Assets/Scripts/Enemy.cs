72714f8d29c8e5c2ff7365e425dd386340d139f2
kevin-liem <reeden88@gmail.com>
2015-07-03 22:42:28 +0700

    imp dead animation

--- Base Defense (Zombie)/Assets/Scripts/Enemy.cs
+++ Base Defense (Zombie)/Assets/Scripts/Enemy.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 	public float speed = 1f;
-//    private bool ableMove = true;
+    private bool ableMove = true;
 	private Transform targetBase;
 	public Transform sightStart, sightEnd;
 	private bool moveToDoor = false;
@@ -12,6 +12,7 @@ public class Enemy : MonoBehaviour {
 	public int hitPoints = 10;
 
     private Animator anim;
+    private int isDeadHash = Animator.StringToHash("isDead");
     private int isAttackingHash = Animator.StringToHash("isAttacking");
 
 	void Start(){
@@ -20,7 +21,7 @@ public class Enemy : MonoBehaviour {
 	}
 
 	void Update () {
-//		if(ableMove){
+		if(ableMove){
 			float step = Time.deltaTime * speed;
 			//use LineCast to see if enemy get contact to invisWall
 			//if got contact to invisWall LayerMask then enemy will move to door instead move toward left
@@ -30,20 +31,29 @@ public class Enemy : MonoBehaviour {
 			if(moveToDoor){
 				//un-Comment debug below to see if Enemy will move to door
 				//Debug.Log("Enemy Move to Door");
-				transform.position = Vector3.MoveTowards(transform.position, targetBase.position, step);					
+				transform.position = Vector3.MoveTowards(transform.position, targetBase.position, step);
 //				ableMove = transform.position != targetBase.position;
 			}else{
 				gameObject.transform.Translate(Vector3.left * Time.deltaTime * speed);
 			}
-//		}
-
-        if (hitPoints <= 0)
-        {
-			Coin coin = GameObject.FindObjectOfType<Coin>();
-			coin.addCoin();
-            Destroy(gameObject);
-        }
+		}
 	}
+
+    void Death()
+    {
+        //make the enemy to stop moving
+        ableMove = false;
+        //let the projectile get through
+        GetComponent<BoxCollider2D>().enabled = false;
+        //untagged this enemy
+        gameObject.tag = "DeadEnemy";
+        //add coin
+        Coin coin = GameObject.FindObjectOfType<Coin>();
+        coin.addCoin();
+        anim.SetTrigger(isDeadHash);
+        //Destory dead enemy after 2s
+        Destroy(gameObject, 3f);
+    }
 
     void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag == "Base"){
@@ -69,5 +79,9 @@ public class Enemy : MonoBehaviour {
 
 	public void Attacked(){
 		hitPoints--;
+        if (hitPoints <= 0)
+        {
+            Death();
+        }
 	}
 }

bc29cc28ef6983d59a185de2d090bebaa36539c8
zipzingame <zipzingame@gmail.com>
2015-07-05 14:42:23 +0700

    add save and Load coin.

--- Base Defense (Zombie)/Assets/Scripts/Coin.cs
+++ Base Defense (Zombie)/Assets/Scripts/Coin.cs
@@ -10,13 +10,19 @@ public class Coin : MonoBehaviour {
 	void Awake()
 	{
 		coinText = GameObject.Find("CoinText").GetComponent<Text>();
+		//Always load coin from dataPlayer
 		countCoin =	DataPlayer.getInstance().coin;
 		coinText.text = countCoin.ToString();
 	}
 
 	public void addCoin(){
 		countCoin += 10;
+		//set coin data in DataPlayer
 		DataPlayer.getInstance().coin = countCoin;
 		coinText.text = countCoin.ToString();
+		//always save coin data for every time get coin
+		//todo in Future will be improved, because can (maybe) be a killer performance
+		//because every state coin will access disk to write file
+		SaveData.Save();
 	}
 }
--- Base Defense (Zombie)/Assets/Scripts/Data Management/SaveData.cs
+++ Base Defense (Zombie)/Assets/Scripts/Data Management/SaveData.cs
@@ -4,12 +4,14 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
 public static class SaveData {
+	//don't change this const ever.. this const for fileName in filesystem
+	private const string saveDataFileName =  "/savedData.gd";
 
 	//it's static so we can call it from anywhere
 	public static void Save() {
 		BinaryFormatter bf = new BinaryFormatter();
 		//Application.persistentDataPath is a string, so if you wanted you can put that into debug.log if you want to know where save games are located
-		FileStream file = File.Create (Application.persistentDataPath + "/savedGames.gd"); //you can call it anything you want
+		FileStream file = File.Create (Application.persistentDataPath + saveDataFileName); //you can call it anything you want
 		bf.Serialize(file, DataPlayer.getInstance());
 		file.Close();
 	}   
@@ -17,7 +19,7 @@ public static class SaveData {
 	public static DataPlayer Load() {
 		if(isHaveData()) {
 			BinaryFormatter bf = new BinaryFormatter();
-			FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+			FileStream file = File.Open(Application.persistentDataPath + saveDataFileName, FileMode.Open);
 			DataPlayer data = (DataPlayer)bf.Deserialize(file);
 			file.Close();
 			return data;
@@ -26,7 +28,8 @@ public static class SaveData {
 	}
 
 	public static bool isHaveData(){
-		return File.Exists(Application.persistentDataPath + "/savedGames.gd");
+		//for check file only, if there is no file in path return false
+		return File.Exists(Application.persistentDataPath +saveDataFileName);
 	}
 }
 	

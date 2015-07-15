3063332ce6a4de068a58a1ab0d452e97fc6cb271
zipzingame <zipzingame@gmail.com>
2015-07-15 21:24:10 +0700

    add databases and data management

---
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Data Management/DataEnemy.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.Temp_Databases.Enemy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class DataEnemy
+{
+    private static DataEnemy instance;
+    private MasterEnemy meleeImp;
+    private MasterEnemy rangedImp;
+    private DataEnemy()
+    {
+        //set status enemy here
+        meleeImp = new MasterEnemy(150, 0, 70, 70, 200);
+        rangedImp = new MasterEnemy(80, 40, 50, 30, 150);
+    }
+    public static DataEnemy getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new DataEnemy();
+        }
+        return instance;
+    }
+    public MasterEnemy MeleeImp
+    {
+        get { return meleeImp; }
+        private set { meleeImp = value; }
+    }
+    public MasterEnemy RangedImp
+    {
+        get { return rangedImp; }
+        private set { rangedImp = value; }
+    }
+}
---
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Temp Databases/Enemy/MasterEnemy.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Temp_Databases.Enemy
+{
+    public class MasterEnemy
+    {
+
+        private int attackSpeed;
+        private int range;
+        private int attackDamage;
+        private int defense;
+        private int hitPoints;
+        //skill & buff?
+
+        public MasterEnemy(int attackSpeed, int range, int attackDamage, int defense, int hitPoints)
+        {
+            this.attackSpeed = attackSpeed;
+            this.range = range;
+            this.attackDamage = attackDamage;
+            this.defense = defense;
+            this.hitPoints = hitPoints;
+        }
+
+
+        public int AttackSpeed
+        {
+            get { return attackSpeed; }
+            private set { attackSpeed = value; }
+        }
+        public int Range
+        {
+            get { return range; }
+            private set { range = value; }
+        }
+        public int AttackDamage
+        {
+            get { return attackDamage; }
+            private set { attackDamage = value; }
+        }
+        public int Defense
+        {
+            get { return defense; }
+            private set { defense = value; }
+        }
+        public int Hitpoints
+        {
+            get { return hitPoints; }
+            private set { hitPoints = value; }
+        }
+    }
+}

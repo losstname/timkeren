f8a661810188b2c5e95b60920a0bc3d0f1b8e49b
kevin-liem <reeden88@gmail.com>
2015-07-28 17:12:42 +0700

    add screen sized sprite tap screen sized sprite to hide skill button

---
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Deprecated/TestTouchedObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTouchedObject : MonoBehaviour {
+
+    public string message;
+
+    void OnMouseDown()
+    {
+        Debug.Log(message);
+    }
+}

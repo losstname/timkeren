df3cdb39656a18f6269d870d822bb692699871fa
kevin-liem <reeden88@gmail.com>
2015-07-31 19:22:49 +0700

    remove ProjectileCollides.cs add ReflectiveArrow.cs (dummy)

---
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Projectiles/ReflectiveArrow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectiveArrow : MonoBehaviour {
+
+    private int countEnemiesHit;
+    private string[] enemiesRealName;
+    private string enemiesHitName;
+    public string targetName;
+
+    void Start()
+    {
+        countEnemiesHit = 0;
+        enemiesHitName = "ReflectiveArrowVictim";
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy" && other.gameObject.name == targetName)
+        {
+            enemiesRealName[countEnemiesHit] = other.gameObject.name;
+            other.gameObject.name = enemiesHitName + countEnemiesHit;
+            countEnemiesHit++;
+            other.gameObject.GetComponent<Enemy>().Attacked();
+        }
+    }
+}

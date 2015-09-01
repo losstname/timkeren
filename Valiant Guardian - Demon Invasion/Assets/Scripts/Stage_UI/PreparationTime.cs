2dec54f308fbc4176b04272d6a57a83d0d7dd689
kevin-liem <reeden88@gmail.com>
2015-09-01 22:50:41 +0700

    Mercenary Shop Panel Hire Mercenary Tab Fire(Dismiss) Mercenary Tab

--- Valiant Guardian - Demon Invasion/Assets/Scripts/Stage_UI/PreparationTime.cs
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Stage_UI/PreparationTime.cs
@@ -12,6 +12,9 @@ public class PreparationTime : MonoBehaviour {
     private GameObject preparationPanelCanvas;
     private GameObject preparationPanel;
     private GameObject trapShop;
+    private GameObject mercenaryShop;
+    private GameObject hireMercenaryTab;
+    private GameObject fireMercenaryTab;
 
     void Awake()
     {
@@ -25,6 +28,13 @@ public class PreparationTime : MonoBehaviour {
 
         //Trap shop panel
         trapShop = GameObject.Find("TrapShop");
+
+        //Mercenary shop panel
+        mercenaryShop = GameObject.Find("MercenaryShop");
+
+        //Hire and Fire tab GO
+        hireMercenaryTab = mercenaryShop.transform.FindChild("HireTab").gameObject;
+        fireMercenaryTab = mercenaryShop.transform.FindChild("FireTab").gameObject;
 
         ResetAllPanel();
         preparationPanelCanvas.SetActive(false);
@@ -74,6 +84,7 @@ public class PreparationTime : MonoBehaviour {
     {
         preparationPanel.SetActive(true);
         trapShop.SetActive(false);
+        mercenaryShop.SetActive(false);
     }
 
     /*------------------------------- Preparation Panel Navigation ---------------------------*/
@@ -83,6 +94,7 @@ public class PreparationTime : MonoBehaviour {
         preparationPanel.SetActive(true);
 
         if (trapShop.activeInHierarchy) { trapShop.SetActive(false); }
+        if (mercenaryShop.activeInHierarchy) { mercenaryShop.SetActive(false); }
     }
 
     public void OpenTrapShop()
@@ -91,5 +103,32 @@ public class PreparationTime : MonoBehaviour {
         trapShop.SetActive(true);
 
         if (preparationPanel.activeInHierarchy) { preparationPanel.SetActive(false); }
+    }
+
+    public void OpenMercenaryShop()
+    {
+        //set the mercenary shop visible
+        mercenaryShop.SetActive(true);
+
+        //Set the first tab to open is hire mercenary tab
+        OpenHireMercenaryTab();
+
+        if (preparationPanel.activeInHierarchy) { preparationPanel.SetActive(false); }
+    }
+
+    public void OpenHireMercenaryTab()
+    {
+        //Hire Mercenary Tab is inside Mercenary Shop
+        hireMercenaryTab.SetActive(true);
+
+        if (fireMercenaryTab.activeInHierarchy) { fireMercenaryTab.SetActive(false); }
+    }
+
+    public void OpenFireMercenaryTab()
+    {
+        //Fire Mercenary Tab is inside Mercenary Shop
+        fireMercenaryTab.SetActive(true);
+
+        if (hireMercenaryTab.activeInHierarchy) { hireMercenaryTab.SetActive(false); }
     }
 }

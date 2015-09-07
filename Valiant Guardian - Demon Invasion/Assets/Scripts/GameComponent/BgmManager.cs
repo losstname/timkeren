561c0d9aa586da55cfe4c9068651e59b5d3a789e
kevin-liem <reeden88@gmail.com>
2015-09-07 23:17:08 +0700

    BgmManager Continue bgm sound through scene

---
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/GameComponent/BgmManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgmManager : MonoBehaviour {
+
+    public static BgmManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+        {
+            //Then destroy instance. This enforces new bgm on current scene.
+            Destroy(instance.gameObject);
+
+            //set instance to this new bgm manager until destroyed again
+            instance = this;
+        }
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+}

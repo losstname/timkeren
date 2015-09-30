22d5bf5ac8ff49885ef1357e9b08a3f7e8d3b546
Brensenzz <brensen_godofwar@yahoo.com>
2015-09-30 23:23:36 +0700

    add sniper skill fullpressure

--- Valiant Guardian - Demon Invasion/Assets/Scripts/Destroy/GrenadeAreaDamage.cs
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Destroy/GrenadeAreaDamage.cs
@@ -13,4 +13,5 @@ public class GrenadeAreaDamage : MonoBehaviour
 			other.GetComponent<Enemy>().AttackedV2();
 		}
 	}
+
 }
--- Valiant Guardian - Demon Invasion/Assets/Scripts/Hero/SkillSniper.cs
+++ Valiant Guardian - Demon Invasion/Assets/Scripts/Hero/SkillSniper.cs
@@ -6,6 +6,8 @@ public class SkillSniper : MonoBehaviour
 
     public GameObject normProjectiles;
     public GameObject ultiProjectiles;
+	public GameObject ultiProjectilesV2;
+	public Vector3 arah2;
 
     private Hero hero;
 
@@ -42,10 +44,30 @@ public class SkillSniper : MonoBehaviour
     {
         //spawning projectile for ultimate skill
         //called from animation
-        Instantiate(ultiProjectiles, hero.ProjectilePosTr.position, hero.ProjectilePosTr.rotation);
-    }
+		GameObject projectile = Instantiate(ultiProjectiles, hero.ProjectilePosTr.position, hero.ProjectilePosTr.rotation) as GameObject;
+		gameObject.GetComponentInParent<HeroSkillTrigger> ().PauseHeroAnimation ();
+		
 
-    public void spawnNormProjectile()
+		projectile.transform.parent = this.transform;
+	}
+
+	public void spawnUltiProjectileV2()
+	{
+		//spawning projectile for ultimate skill
+		//called from animation
+		GameObject projectile = Instantiate(ultiProjectilesV2, hero.ProjectilePosTr.position, hero.ProjectilePosTr.rotation) as GameObject;
+		//projectile.transform.parent = this.transform;
+
+		//Quaternion direction = Quaternion.RotateTowards(projectile.transform.rotation,ara)  //Quaternion.LookRotation(projectile.transform.forward, projectile.transform.TransformDirection(Vector3.up));
+
+		//set the laser direction
+		Vector3 targetDir = arah2 - transform.position;
+		float angle = Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg;
+		projectile.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		//projectile.GetComponent<Transform> ().rotation = new Quaternion(0, 0, direction.z, direction.w);
+	}
+
+	public void spawnNormProjectile()
     {
         //spawning projectile
         //called from animation
